6306cdef2a9f7e48c5421bd6e15195160756d4e2
Mateus Correia <mateus.correia@outlook.com>
2022-05-28 23:47:25 -0300

    a new commit in the project - remove some files that is not important now

--- App.js
+++ App.js
@@ -80,7 +80,7 @@ const App: () => Node = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Read the docs to discover what to do:
           </Section>
           <LearnMoreLinks />
         </View>
